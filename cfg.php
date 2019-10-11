4111dfcc018027240be2765915539be5eae7cdac
jethscarlet <56187664+jethscarlet@users.noreply.github.com>
2019-10-12 00:42:33 +0800

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,10 @@
+<?php
+
+//MASUKAN D_INFO MILIK KALIAN!!
+$d_info = "xxxxxxxxxx";
+
+//MASUKAN TOKEN MILIK KALIAN!!
+$token = "xxxxxxxxxxx";
+
+//MASUKAN SESSION MILIK KALIAN!!
+$session = "xxxxxxxxxx";
